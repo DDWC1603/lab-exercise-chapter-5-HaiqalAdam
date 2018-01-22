1547e6eef6f1555346a71feef44f35264801df8d
HaiqalAdam <33884196+HaiqalAdam@users.noreply.github.com>
2018-01-22 18:26:59 +0800

    Update Exercise-Function-1.cpp

--- Exercise-Function-1.cpp
+++ Exercise-Function-1.cpp
@@ -1,5 +1,7 @@
 //this is an uncomple program with function in it, please complete this program. thank you.
 //make the program so that when the user enter 'n' three time then it will break and print "the fudge!?"
+//MUHAMMAD HAIQAL ADAM BIN MOHD HIRWAN
+//A17DW0071
 
 #include <iostream>
 using namespace std;
@@ -7,28 +9,27 @@ using namespace std;
 
 int main()
 {
-}
-
-void masuk_tak()
-{
-	char ans;
+	char y , n , ans;
 	
-	cout<<endl<<"Dia shoot masuk tak? (answer y-yes atau n-tidak"<<endl;
+	cout<<endl<<"Dia shoot masuk tak? answer yes atau no"<<endl;
+	cin>>y;
+	cin>>n;
 	
-
-	ans=' ';
-	while(ans!='y'  ans!='y'){
+	ans='y';
+	while(ans!='y')  (ans!='y');{
 		cin>>ans;
-		if(ans!='n'  ans!='n')
+		if(ans!='n')  (ans!='n');
 		cout<<endl<<"cakap lah bebetul, try again: "<<endl;
 		}
 
 	if(ans='n')
 	{
 		cout<<endl<<"the fudge?!";
-		masuk_tak();
+		cin>>ans;
 	}
 	cout<<endl<<"chantek!"<<endl;
+	
 }
 
-
+//updated
+//signed by haiqal
