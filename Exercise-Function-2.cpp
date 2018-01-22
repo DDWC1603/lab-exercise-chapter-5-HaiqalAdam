c294b4238ad7cbe1fa65763fb2541a6cc988a49d
HaiqalAdam <33884196+HaiqalAdam@users.noreply.github.com>
2018-01-22 18:47:14 +0800

    Update Exercise-Function-2.cpp

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -2,10 +2,31 @@
 //finish up this program
 //take the value from x and y and sum it up.
 //this is a function related exercise, it need to be in a way where you put function in your program
-
-int sum(int x,int y)
+//HAIQAL ADAM
+//A17DW0071
+#include <iostream>
+using namespace std;
+int sum();
+	int sum(int x,int y)
 {
+
 	int result;
 	result = x+y;
+	cout<<result;
+	return x+y;
+	 	
 
 }
+int main()
+{
+	int x;
+	int y;
+	cin>>x;
+	cin>>y;
+ 	sum(x , y);
+ 	return 0;
+
+}
+
+//updated
+//signed by haiqal
